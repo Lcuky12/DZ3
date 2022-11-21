64766e1c9bac5095959ad4aac836d95a776a41e5
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2022-11-21 21:42:36 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write(" Как Вас зовут? ");
+            name = Convert.ToString(Console.ReadLine());
+            int age;
+            Console.Write(" Сколько Вам лет? ");
+            age = Convert.ToInt32(Console.ReadLine());
+            string sign;
+            Console.Write(" Кто Вы по знаку зодиака? ");
+            sign = Convert.ToString(Console.ReadLine());
+            string work;
+            Console.Write(" Где Вы работаете? ");
+            work = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(" Вас зовут " + name + ',' + " Вам " + age + " год " + ',' + " вы " + sign + ',' + " работаете на " + work + '.');
+        }
+    }
+}
